259bd08545a855c9f9f0793cf72ebfd31fe1fdfb
Alexander Gnatenko <agnatenko@gmail.com>
2021-03-15 23:19:36 +0300

    Решение задачи

--- src/main/java/ru/idcore/ClientMain.java
+++ src/main/java/ru/idcore/ClientMain.java
@@ -7,11 +7,10 @@ import java.util.Scanner;
 public class ClientMain {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("localhost", 17117);
-        //System.out.println("Client is running...");
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter writer = new PrintWriter(
-                    new OutputStreamWriter(socket.getOutputStream()), true);
-            Scanner scanner = new Scanner(System.in)) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter writer = new PrintWriter(
+                     new OutputStreamWriter(socket.getOutputStream()), true);
+             Scanner scanner = new Scanner(System.in)) {
 
             String msg;
             while (true) {
@@ -19,7 +18,7 @@ public class ClientMain {
                 msg = scanner.nextLine();
                 writer.println(msg);
 
-                if(msg.equals("end")) {
+                if (msg.equals("end")) {
                     break;
                 }
                 System.out.println("SERVER: " + reader.readLine());
